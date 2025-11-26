00a254f4b7f3c85895bec4984ce1fe92404b3e63
mzainakram1234-fsd <m.akramzain1234@gmail.com>
2025-11-26 21:32:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+ var Typed1 = new Typed('#element', {
+      strings: ['MUHAMMAD ZAIN AKRAM....', 'MUHAMMAD ZAIN AKRAM.... '],
+      typeSpeed: 100,
+      backSpeed : 50,
+      loop : true ,
+
+    });
+    
+    var typed2 = new Typed('#element2', {
+      strings: ['<span class="element2color">FULL STACK WEB DEVELOPER...</span>','<span class="element2color1">HTML</span>','<span class="element2color2">CSS</span>', '<span class="element2color3">JAVASCRIPT</span>','<span class="element2color4">BOOTSTRAP</span>','<span class="element2color5">PHP</span>','<span class="element2color6">MYSQL......</span>',],
+      typeSpeed: 60,
+      backSpeed: 20,
+      loop: true,
+      //  html: true
+    });
+      var Typed3 = new Typed('.element3', {
+           strings: ['MY PROJECTS', 'MY PROJECTS '],
+           typeSpeed: 100,
+           backSpeed : 50,
+           loop : true ,
+     
+         });
+      var Typed4 = new Typed('.element4', {
+           strings: ['CHECK IT AND', 'CHECK IT AND'],
+           typeSpeed: 100,
+           backSpeed : 50,
+           loop : true ,
+     
+         });
+         
